d1c8c722d759a15d93b0e14d04ef9789aeec8b4c
Srikar Mahesh <ysrikarmahesh@usharama.in>
2026-07-07 09:42:21 +0530

    Subtraction Program

--- code.py
+++ code.py
@@ -1,10 +1,5 @@
 print("Hello")
 print("How are you?")
-# Addition Program
-d=10
-e=20
-f=d+e
-print("The sum of d and e is:", f)
 # Subtraction Program
 print("This is an Simple Subtraction Program")
 a = 50

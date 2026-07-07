1ac73f38b137db268195563799b387a404bb718f
Srikar Mahesh <ysrikarmahesh@usharama.in>
2026-07-07 09:43:58 +0530

    Additon Program

--- code.py
+++ code.py
@@ -5,9 +5,3 @@ d=10
 e=20
 f=d+e
 print("The sum of d and e is:", f)
-# Subtraction Program
-print("This is an Simple Subtraction Program")
-a = 50
-b = 20
-c = a - b
-print("The difference of a and b is:", c)
